3c2fc0bc70b0a1477630bdb580563824b01150a6
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-06 04:21:15 +0100

    End of Section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query(), ct);
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]//activites /id
