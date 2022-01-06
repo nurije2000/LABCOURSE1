1bc3aa12fb612079e7fd907aee675e83a32491f5
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-06 04:19:20 +0100

    End of Section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,6 @@
-
-
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -8,6 +8,8 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController: ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
---
+++ Application/Activities/List.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly Datacontext _context;
+            public Handler(Datacontext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
