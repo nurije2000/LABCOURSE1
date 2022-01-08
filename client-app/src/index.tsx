f4ea6101508a14cef4437e01fe1c5627035d48ce
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-08 03:25:02 +0100

    End of Section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css'
+import './app/layout/styles.css';
+
 import reportWebVitals from './reportWebVitals';
+import App from './app/layout/App';
 
 ReactDOM.render(
+ 
     <App />,
+ 
   document.getElementById('root')
 );
 
