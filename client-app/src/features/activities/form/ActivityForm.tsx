532a4e8157e55c6af6c96c81b04304bb365e5d18
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-08 20:40:28 +0100

    End of Section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default  function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props){
+export default  function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props){
      
     
   const initialState = selectedActivity ?? {
@@ -40,10 +41,10 @@ setActivity({...activity, [name]: value})
                 <Form.Input placeholder='Title' value={activity.title} name='title'onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description' />
                 <Form.Input placeholder='Category' />
-                <Form.Input placeholder='Date' />
+                <Form.Input type='date' placeholder='Date' />
                 <Form.Input placeholder='City' />
                 <Form.Input placeholder='Venue' />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right'  type='button' content='Cancel' />
             </Form>
         </Segment>
