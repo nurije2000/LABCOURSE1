bcba0462e23607cf91ff5a150c0f065c28b46d55
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-12 00:49:22 +0100

    End of Section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -21,6 +21,16 @@ loadingInitial = true;
     Date.parse(a.date) - Date.parse(b.date));
     }
 
+    get groupedActivities(){
+return Object.entries(
+    this.activitiesByDate.reduce((activities, activity) =>{
+        const date = activity.date;
+        activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        return activities;
+    }, {} as {[key: string]: Activity[]} )
+)
+    }
+
     loadActivities = async () => {
         this.loadingInitial = true;
         try{
