5f76d406d47ded341af9a8f4d82b7928eaae42c0
nurije osmani <nurije.osmani2000@outlook.com>
2022-01-14 15:28:03 +0100

    End of Section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,21 +13,22 @@ namespace API.Controllers
        
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]//activites /id
-        public async Task<ActionResult<Activity>> GetActivities(Guid id)
+        public async Task<IActionResult> GetActivities(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id = id});
+
+            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
         }
          
          [HttpPost]
          public async Task<IActionResult> CreateActivity(Activity activity)
          {
-             return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+             return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
          }
 
          [HttpPut("{id}")]
@@ -36,12 +37,12 @@ namespace API.Controllers
          {
 
              activity.Id = id;
-             return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+             return HandleResult(await Mediator.Send(new Edit.Command{Activity = activity}));
          }
          [HttpDelete("{id}")]
          public async Task<IActionResult> DeleteActivity(Guid id)
          {
-             return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
          }
               
     }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,34 +1,56 @@
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
+using Activity = Domain.Activity;
 
 namespace Application.Activities
 {
     public class Create
     {
-
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly Datacontext _context;
+
             public Handler(Datacontext context)
             {
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+               var result = await _context.SaveChangesAsync() > 0;
+                if (!result) return Result<Unit>.Failure ("Failed to create activity!");
 
-                return Unit.Value;
+                return Result<Unit>.Success(Unit.Value);
+            }
+
+            private string GetDebuggerDisplay()
+            {
+                return ToString();
             }
         }
+
+        // public class Command : IRequest<object>
+        // {
+        //     public Activity Activity { get; set; }
+        // }
     }
 }
